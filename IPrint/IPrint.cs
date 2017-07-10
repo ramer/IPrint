3c63d024678931469356e12abaf6fde2207a8884
sorokovikov <sorokovikov@WS-NG-100>
2017-07-10 12:44:54 +0300

    Добавьте файлы проекта.

---
+++ IPrint/IPrint.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Documents;
+
+namespace IPrint
+{
+    public class IPrint
+    {
+        public static bool ShowPreview (FlowDocument fd)
+        {
+
+
+            return true;
+        }
+
+        public static bool Print(FlowDocument fd)
+        {
+
+
+            return true;
+        }
+    }
+}
